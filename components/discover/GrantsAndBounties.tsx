9eaf1b62b888cc24c5e78462433618b2c14d5147
Andrei Tapurin <tapurin.andrei@gmail.com>
2022-03-13 18:35:06 +0000

    GrantsAndBounties

---
+++ components/discover/GrantsAndBounties.tsx
@@ -0,0 +1,23 @@
+import { FormattedMessage, useIntl } from "react-intl";
+import { getIconComponentByName } from "../../utils/icons-map";
+import Button from "../Button";
+import { DiscoverErgHero } from "../icons";
+
+export default function GrantsAndBounties() {
+  const intl = useIntl();
+  const button1Text = intl.formatMessage({ id: 'components.discoverHero.button1', defaultMessage: 'DEV BOUNTIES' });
+  const button2Text = intl.formatMessage({ id: 'components.discoverHero.button2', defaultMessage: 'GRANTS' });
+  const button3Text = intl.formatMessage({ id: 'components.discoverHero.button3', defaultMessage: 'BUG BOUNTIES' });
+
+  return (
+    <div className="max-w-[1300px] mx-auto p-4 mt-40">
+      <h1 className="leading-tight xl:ml-72"><b>Grants & Bounties</b></h1>
+      <p className="max-w-xs text-[#989898] dark:text-[#989898] mt-6 xl:mt-24 xl:ml-[600px] xl:max-w-lg">We are looking to fund grants for projects that focus on building key ecosystem infrastructure. The proposals, at the moment, need to be approved by four of six votes of Ergo Development Fund Board Members. Please reference this article for scoping your own grant, and send it to team@ergoplatform.org.</p>
+      <div className="flex flex-col xl:flex-row xl:ml-[600px]">
+        <div className="-ml-4 mt-10"><Button text={button1Text} url='/url' newTab={true} underline={true} textColor="brand-orange" background={false} icon='ArrowRight' iconColor='orange' /></div>
+        <div className="-ml-4 mt-10 xl:ml-16"><Button text={button2Text} url='/url' newTab={true} underline={true} textColor="brand-orange" background={false} icon='ArrowRight' iconColor='orange' /></div>
+        <div className="-ml-4 mt-10 xl:ml-16"><Button text={button3Text} url='/url' newTab={true} underline={true} textColor="brand-orange" background={false} icon='ArrowRight' iconColor='orange' /></div>
+      </div>
+    </div>
+  );
+}
--- pages/discover.tsx
+++ pages/discover.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import { FormattedMessage, useIntl } from 'react-intl'
 import DiscoverHero from '../components/discover/DiscoverHero';
+import GrantsAndBounties from '../components/discover/GrantsAndBounties';
 import Layout from '../components/Layout'
 
 const Discover: NextPage = () => {
@@ -9,6 +10,7 @@ const Discover: NextPage = () => {
   return (
     <Layout title={title}>
       <DiscoverHero />
+      <GrantsAndBounties />
     </Layout>
   )
 }
