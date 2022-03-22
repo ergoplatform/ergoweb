6aa28a91487908f41042a2e775fd7da6d392c04b
Andrei Tapurin <tapurin.andrei@gmail.com>
2022-03-22 09:24:39 +0000

    DApps

--- pages/ecosystem.tsx
+++ pages/ecosystem.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from "next";
 import { FormattedMessage, useIntl } from "react-intl";
+import DApps from "../components/ecosystem/DApps";
 import EcosystemHero from "../components/ecosystem/DiscoverHero";
 import Layout from "../components/Layout";
 
-const Ecosystem: NextPage = () => {
+type Props = {
+  apps?: any;
+};
+
+export default function Ecosystem(props: Props) {
   const intl = useIntl();
   const title = intl.formatMessage({
     id: "pages.ecosystem.title",
@@ -12,8 +17,20 @@ const Ecosystem: NextPage = () => {
   return (
     <Layout title={title}>
       <EcosystemHero />
+      <DApps apps={props.apps}/>
     </Layout>
   );
 };
 
-export default Ecosystem;
+export const getServerSideProps = async (context: any) => {
+  const apps = await fetch(
+    process.env.NEXT_PUBLIC_STRAPI_API +
+      "/api/dapps?populate=*&locale=" +
+      context.locale
+  ).then((response) => response.json());
+  console.log(apps)
+  return {
+    props: { apps },
+  };
+};
+
