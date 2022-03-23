a15744a453da7af6db80467f62ff41b5ff4c5d69
Andrei Tapurin <tapurin.andrei@gmail.com>
2022-03-23 21:00:08 +0000

    FeaturesAndProjects small screen

---
+++ components/ecosystem/FeaturesAndProjects.tsx
@@ -0,0 +1,157 @@
+type Props = {
+  projects?: any;
+};
+
+export default function FeaturesAndProjects(props: Props) {
+  return (
+    <div className="max-w-[1300px] mx-auto p-4 relative">
+      <p className="text-[#585858] dark:text-[#585858] font-bold mb-8">
+        NEW FEATURES AND PROJECTS YOU MIGHT LIKE
+      </p>
+      <div className="flex overflow-x-auto no-scrollbar gap-5">
+        {props.projects.data.map((project: any, i: number) => (
+          <div className="rounded-3xl features-and-projects-bg flex flex-col min-w-[282px] p-4">
+            <img
+              className="object-contain items-center h-1/2"
+              src={project.attributes.image.data?.attributes.url}
+            />
+            <div className="h-1/2 flex flex-col">
+              <p className="mt-4 text-[#585858] dark:text-[#585858] lg:text-[20px] font-bold h-1/5">
+                {project.attributes.group}
+              </p>
+              <p className="mt-2 font-subtitle-3-bold lg:text-[24px] h-2/5">
+                {project.attributes.title}
+              </p>
+              <p className="mt-2 text-[#989898] dark:text-[#989898] lg:text-[16px] h-2/5">
+                {project.attributes.description}
+              </p>
+              <div className="my-2 h-1/5">
+                <a
+                  className="underline text-brand-orange dark:text-brand-orange font-vinila-extended lg:text-[16px]"
+                  href={project.attributes.url}
+                  target="_blank"
+                >
+                  {project.attributes.button_text}
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* <div className="flex overflow-x-auto no-scrollbar pb-10 lg:grid lg:grid-cols-3 lg:grid-rows-2 gap-8 max-h-[860px]">
+        <div className="px-6 py-10 rounded-3xl box lg:row-span-2 features-and-projects-bg flex lg:flex-col justify-around">
+          <div>
+            <img
+              className="object-fit max-h-[500px] items-center"
+              src={props.projects.data[0].attributes.image.data?.attributes.url}
+            />
+          </div>
+          <div>
+            <p className="mt-4 text-[#585858] dark:text-[#585858] lg:text-[20px] font-bold">
+              {props.projects.data[0].attributes.group}
+            </p>
+            <p className="mt-2 font-subtitle-3-bold lg:text-[24px]">
+              {props.projects.data[0].attributes.title}
+            </p>
+            <p className="mt-2 text-[#989898] dark:text-[#989898] lg:text-[16px]">
+              {props.projects.data[0].attributes.description}
+            </p>
+            <div className="my-2">
+              <a
+                className="underline text-brand-orange dark:text-brand-orange font-vinila-extended lg:text-[16px]"
+                href={props.projects.data[0].attributes.url}
+                target="_blank"
+              >
+                {props.projects.data[0].attributes.button_text}
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="p-10 rounded-3xl box lg:col-span-2 features-and-projects-bg flex flex-row ">
+          <div className="my-auto w-1/2">
+            <img
+              className="object-fit items-center max-h-[300px]"
+              src={props.projects.data[1].attributes.image.data?.attributes.url}
+            />
+          </div>
+          <div className="my-auto ml-6  w-1/2">
+            <p className="mt-4 text-[#585858] dark:text-[#585858] lg:text-[20px] font-bold">
+              {props.projects.data[1].attributes.group}
+            </p>
+            <p className="mt-2 font-subtitle-3-bold lg:text-[24px]">
+              {props.projects.data[1].attributes.title}
+            </p>
+            <p className="mt-2 text-[#989898] dark:text-[#989898] lg:text-[16px]">
+              {props.projects.data[1].attributes.description}
+            </p>
+            <div className="my-2">
+              <a
+                className="underline text-brand-orange dark:text-brand-orange font-vinila-extended lg:text-[16px]"
+                href={props.projects.data[1].attributes.url}
+                target="_blank"
+              >
+                {props.projects.data[1].attributes.button_text}
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="p-10 rounded-3xl box features-and-projects-bg flex lg:flex-col justify-around">
+          <div>
+            <img
+              className="object-fit items-center max-h-[200px]"
+              src={props.projects.data[2].attributes.image.data?.attributes.url}
+            />
+          </div>
+          <div>
+            <p className="mt-4 text-[#585858] dark:text-[#585858] lg:text-[20px] font-bold">
+              {props.projects.data[2].attributes.group}
+            </p>
+            <p className="mt-2 font-subtitle-3-bold lg:text-[24px]">
+              {props.projects.data[2].attributes.title}
+            </p>
+            <p className="mt-2 text-[#989898] dark:text-[#989898] lg:text-[16px]">
+              {props.projects.data[2].attributes.description}
+            </p>
+            <div className="my-2">
+              <a
+                className="underline text-brand-orange dark:text-brand-orange font-vinila-extended lg:text-[16px]"
+                href={props.projects.data[2].attributes.url}
+                target="_blank"
+              >
+                {props.projects.data[2].attributes.button_text}
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="p-10 rounded-3xl box features-and-projects-bg flex lg:flex-col justify-around">
+          <div>
+            <img
+              className="object-fit items-center max-h-[200px]"
+              src={props.projects.data[3].attributes.image.data?.attributes.url}
+            />
+          </div>
+          <div>
+            <p className="mt-4 text-[#585858] dark:text-[#585858] lg:text-[20px] font-bold">
+              {props.projects.data[3].attributes.group}
+            </p>
+            <p className="mt-2 font-subtitle-3-bold lg:text-[24px]">
+              {props.projects.data[3].attributes.title}
+            </p>
+            <p className="mt-2 text-[#989898] dark:text-[#989898] lg:text-[16px]">
+              {props.projects.data[3].attributes.description}
+            </p>
+            <div className="my-2">
+              <a
+                className="underline text-brand-orange dark:text-brand-orange font-vinila-extended lg:text-[16px]"
+                href={props.projects.data[3].attributes.url}
+                target="_blank"
+              >
+                {props.projects.data[3].attributes.button_text}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
+}
--- pages/ecosystem.tsx
+++ pages/ecosystem.tsx
@@ -1,12 +1,14 @@
 import { useIntl } from "react-intl";
 import DApps from "../components/ecosystem/DApps";
 import EcosystemHero from "../components/ecosystem/DiscoverHero";
+import FeaturesAndProjects from "../components/ecosystem/FeaturesAndProjects";
 import Roadmap from "../components/ecosystem/Roadmap";
 import Layout from "../components/Layout";
 
 type Props = {
   apps?: any;
   roadmap?: any;
+  projects?: any;
 };
 
 export default function Ecosystem(props: Props) {
@@ -20,6 +22,7 @@ export default function Ecosystem(props: Props) {
       <EcosystemHero />
       <DApps apps={props.apps}/>
       <Roadmap roadmapItems={props.roadmap}/>
+      <FeaturesAndProjects projects={props.projects} />
     </Layout>
   );
 };
@@ -35,8 +38,13 @@ export const getServerSideProps = async (context: any) => {
       "/api/roadmaps?populate=*&locale=" +
       context.locale
   ).then((response) => response.json());
+  const projects = await fetch(
+    process.env.NEXT_PUBLIC_STRAPI_API +
+      "/api/features-and-projects?populate=*&pagination[page]=1&pagination[pageSize]=4&locale=" +
+      context.locale
+  ).then((response) => response.json());
   return {
-    props: { apps, roadmap },
+    props: { apps, roadmap, projects },
   };
 };
 
