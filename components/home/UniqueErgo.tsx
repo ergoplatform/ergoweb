f5f03e791615cb72f0078b1992108567738eda1b
Andrei Tapurin <tapurin.andrei@gmail.com>
2022-04-06 22:09:26 +0100

    unique ergo mobile

--- components/home/UniqueErgo.tsx
+++ components/home/UniqueErgo.tsx
@@ -54,39 +54,41 @@ export default function UniqueErgo() {
 
   return (
   
-    <div className="mt-28 relative z-20 overflow-x-auto no-scrollbar">
+    <div className="mt-28 ">
       <div className="text-center"><p className="font-bold text-[#585858] dark:text-[#989898] text-[16px] lg:text-[20px]">WHAT MAKES ERGO UNIQUE</p></div>
-      <div className="antialiased flex items-center justify-center my-8 min-w-[1000px]">
-        <div className="flex h-[638px] items-stretch min-w-md overflow-hidden w-full">
-          {cards.map((card, index) => (
-            <div
-              key={index}
-              className={"bg-center bg-cover bg-[#FCFCFC] dark:bg-brand-black card cursor-pointer flex-grow min-w-14 overflow-hidden relative transition-base border-[1px] border-[#625d5d] dark:border-[#2A2A2A]" + (index === 0 ? " active" : "")}
-            >
-              <div className="absolute top-16 lg:top-8 flex label mb-3 ml-4 lg:ml-24 transition-base z-20 text-[16px] lg:text-[24px] flex-col">
-                <div className="hidden dark:block">{card.icon}</div>
-                <div className="dark:hidden">{card.iconLight}</div>
-                <div className="mt-8 max-w-[300px]">{card.title}</div>
+      <div className="relative z-20 overflow-x-auto no-scrollbar">
+        <div className="antialiased flex items-center justify-center my-8 min-w-[1000px]">
+          <div className="flex h-[638px] items-stretch min-w-md overflow-hidden w-full">
+            {cards.map((card, index) => (
+              <div
+                key={index}
+                className={"bg-center bg-cover bg-[#FCFCFC] dark:bg-brand-black card cursor-pointer flex-grow min-w-14 overflow-hidden relative transition-base border-[1px] border-[#625d5d] dark:border-[#2A2A2A]" + (index === 0 ? " active" : "")}
+              >
+                <div className="absolute top-16 lg:top-8 flex label mb-3 ml-4 lg:ml-24 transition-base z-20 text-[16px] lg:text-[24px] flex-col">
+                  <div className="hidden dark:block">{card.icon}</div>
+                  <div className="dark:hidden">{card.iconLight}</div>
+                  <div className="mt-8 max-w-[300px]">{card.title}</div>
+                </div>
+                <div className="absolute bottom-0 from-transparent h-1/2 inset-x-0 temphide hidden to-black transform transition-base translate-y-1/2 z-10 text-[#585858] dark:text-[#989898] max-w-xs lg:max-w-sm ml-6 lg:ml-[106px]">
+                  <div>{card.text}</div>
+                  {card.url !== undefined ? (
+                    <div className="-ml-4 mt-12 sm:mt-10 lg:mt-16">
+                      <Button
+                        text={card.buttoText}
+                        url={card.url}
+                        newTab={true}
+                        underline={true}
+                        textColor="brand-orange"
+                        background={false}
+                        icon="ArrowRight"
+                        iconColor="orange"
+                      />
+                    </div>
+                  ) : null}
+                </div>
               </div>
-              <div className="absolute bottom-0 from-transparent h-1/2 inset-x-0 temphide hidden to-black transform transition-base translate-y-1/2 z-10 text-[#585858] dark:text-[#989898] max-w-xs lg:max-w-sm ml-6 lg:ml-[106px]">
-                <div>{card.text}</div>
-                {card.url !== undefined ? (
-                  <div className="-ml-4 mt-12 sm:mt-10 lg:mt-16">
-                    <Button
-                      text={card.buttoText}
-                      url={card.url}
-                      newTab={true}
-                      underline={true}
-                      textColor="brand-orange"
-                      background={false}
-                      icon="ArrowRight"
-                      iconColor="orange"
-                    />
-                  </div>
-                ) : null}
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
