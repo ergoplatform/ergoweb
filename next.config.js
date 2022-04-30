3e8734e751bb0cdf253e9f5cc2334141c09cb88b
Andrei Tapurin <tapurin.andrei@gmail.com>
2022-04-30 16:02:02 +0100

    setup redirects

--- next.config.js
+++ next.config.js
@@ -7,16 +7,84 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/docs/:path*',
-        destination: 'https://storage.googleapis.com/ergo-cms-media/docs/:path*',
-        permanent: false,
+        source: "/docs/:path*",
+        destination:
+          "https://storage.googleapis.com/ergo-cms-media/docs/:path*",
+        permanent: true,
       },
       {
-        source: '/img/:path*',
-        destination: 'https://storage.googleapis.com/ergo-cms-media/img/:path*',
-        permanent: false,
+        source: "/img/:path*",
+        destination: "https://storage.googleapis.com/ergo-cms-media/img/:path*",
+        permanent: true,
       },
-    ]
+
+      {
+        source: "/about/",
+        destination: "/discover/#DiscoverERG",
+        permanent: true,
+      },
+      {
+        source: "/basics/",
+        destination: "/discover/#DiscoverERG",
+        permanent: true,
+      },
+      {
+        source: "/wallets/",
+        destination: "/get-erg/#Wallets",
+        permanent: true,
+      },
+      {
+        source: "/exchanges/",
+        destination: "/get-erg/#Exchanges",
+        permanent: true,
+      },
+      {
+        source: "/contribute/",
+        destination: "/community/#Contribute",
+        permanent: true,
+      },
+      {
+        source: "/software/",
+        destination: "/discover/#DiscoverERG",
+        permanent: true,
+      },
+      {
+        source: "/use_cases/",
+        destination: "/#UniqueErgo",
+        permanent: true,
+      },
+      {
+        source: "/foundation/",
+        destination: "/community/#Foundation",
+        permanent: true,
+      },
+      {
+        source: "/hall_of_fame/",
+        destination: "/community/#HallOfFame",
+        permanent: true,
+      },
+      {
+        source: "/news/",
+        destination: "/blog/",
+        permanent: true,
+      },
+      {
+        source: "/documents/",
+        destination: "/discover/#Documents",
+        permanent: true,
+      },
+      { source: "/faq/", destination: "/discover/#FAQ", permanent: true },
+      {
+        source: "/mining/",
+        destination: "/get-erg/#Mining",
+        permanent: true,
+      },
+      {
+        source: "/privacy/",
+        destination: "/privacy-policy/",
+        permanent: true,
+      },
+    ];
   },
   reactStrictMode: true,
   trailingSlash: true,
