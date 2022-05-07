c291eb3fee237de2116c516335c0cfb5494adf8a
Andrei Tapurin <tapurin.andrei@gmail.com>
2022-05-07 18:10:41 +0100

    blog page make news clickable

--- components/blog/BlogNews.tsx
+++ components/blog/BlogNews.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FormattedDate } from "react-intl";
 import Button from "../Button";
 
@@ -10,39 +11,46 @@ type NewsPostProps = {
 
 function NewsPost({ title, subtitle, date, url }: NewsPostProps) {
   return (
-    <div className="min-w-[300px] h-[300px] rounded-xl p-6 md:px-20 md:py-16 blog-news relative md:min-w-[610px] md:h-[400px]">
-      <p className="text-[12px] md:text-[14px]">
-        <b>
-          <FormattedDate
-            value={date}
-            day="numeric"
-            month="long"
-            year="numeric"
-          />
-        </b>
-      </p>
-      <p className="text-[16px] md:text-[24px] font-subtitle-3-bold text-brand-orange dark:text-brand-orange my-2 text-clip">
-        {title.length > 60 ? title.substring(0, 60) + "..." : title}
-      </p>
-      {subtitle ? (
-        <p className="text-[12px] md:text-[16px] text-[#807e7e] dark:text-[#807e7e] mb-2 text-clip">
-          {subtitle?.length > 350 ? subtitle?.substring(0, 320) + "..." : subtitle}
-        </p>
-      ) : (
-        ""
-      )}
-
-      <div className="-ml-4 absolute bottom-4 md:bottom-12">
-        <Button
-          text="READ"
-          url={url}
-          newTab={true}
-          underline={true}
-          textColor="brand-orange"
-          background={false}
-        />
-      </div>
-    </div>
+    <>
+      {url ? (
+        <Link key={url} href={url}>
+          <div className="min-w-[300px] h-[300px] rounded-xl p-6 md:px-20 md:py-16 blog-news relative md:min-w-[610px] md:h-[400px] cursor-pointer">
+            <p className="text-[12px] md:text-[14px]">
+              <b>
+                <FormattedDate
+                  value={date}
+                  day="numeric"
+                  month="long"
+                  year="numeric"
+                />
+              </b>
+            </p>
+            <p className="text-[16px] md:text-[24px] font-subtitle-3-bold text-brand-orange dark:text-brand-orange my-2 text-clip">
+              {title.length > 60 ? title.substring(0, 60) + "..." : title}
+            </p>
+            {subtitle ? (
+              <p className="text-[12px] md:text-[16px] text-[#807e7e] dark:text-[#807e7e] mb-2 text-clip">
+                {subtitle?.length > 350
+                  ? subtitle?.substring(0, 320) + "..."
+                  : subtitle}
+              </p>
+            ) : (
+              ""
+            )}
+            <div className="-ml-4 absolute bottom-4 md:bottom-12">
+              <Button
+                text="READ"
+                url={url}
+                newTab={true}
+                underline={true}
+                textColor="brand-orange"
+                background={false}
+              />
+            </div>
+          </div>
+        </Link>
+      ) : null}
+    </>
   );
 }
 
