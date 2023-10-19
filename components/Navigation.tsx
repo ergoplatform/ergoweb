a5a7f15bfad9c740f28cfc794f61d51321b048ae
M Glasgow <markglasgow@gmail.com>
2023-10-19 20:43:17 +0100

    discords

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -399,7 +399,11 @@ function Navigation({ enableLanguages = true }) {
                     </a>
                   </div>
                   <div className="box mx-auto my-auto">
-                    <a href="https://discord.gg/RC8M4Bkgsy" target="_blank" rel="noreferrer">
+                    <a
+                      href="https://discord.gg/ergo-platform-668903786361651200"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
                       {getIconComponentByName('Discord')}
                     </a>
                   </div>
