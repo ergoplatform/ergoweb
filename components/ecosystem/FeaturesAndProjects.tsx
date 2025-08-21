76c171af3b2444cc58e3f7d4ab7b0098cb7b6192
M Glasgow <markglasgow@gmail.com>
2025-08-21 13:50:22 +0100

    fix: remove clamp function from project titles

--- components/ecosystem/FeaturesAndProjects.tsx
+++ components/ecosystem/FeaturesAndProjects.tsx
@@ -71,9 +71,9 @@ export default function FeaturesAndProjects(props: Props) {
               <p className="mt-4 text-[#585858] dark:text-[#585858] lg:text-[20px] font-bold h-1/5">
                 {project.attributes.group}
               </p>
-              <h3 className="mt-2 font-subtitle-3-bold text-[clamp(40px,10vw,80px)] h-2/5 heading-balance">
+              <h4 className="mt-2 font-subtitle-3-bold heading-balance">
                 {project.attributes.title}
-              </h3>
+              </h4>
               <p className="mt-2 text-[#807e7e] dark:text-[#807e7e] lg:text-[16px] h-2/5">
                 {project.attributes.description}
               </p>
